960b12b14043b30b8374804334637c2a6e3c5548
ejemplogithub <pruebacorreogit@gmail.com>
2016-08-15 17:37:02 -0600

    Creamos dos programas uno para sumar dos numeros y el otro usando C para explicar el método de ordenamiento de vectores BUBBLE SORT

---
+++ programa.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float n1, n2, sum;
+	cout << "Ingresa dos numeros: ";
+	cin >> n1 >> n2;
+	sum = n1+n2;
+	cout << "Sum = " << sum;
+	return 0;
+}
